d0fa32c13521b5feafa79c7493a974451d7eb9de
tungwithconga <147390524+tungwithconga@users.noreply.github.com>
2024-04-04 08:04:38 +0700

    Add files via upload

---
+++ bai1.js
@@ -0,0 +1,9 @@
+console.log("wellcome to Rikkei Academy");
+document.write("Quyết tâm học lập trình javascript");
+alert("Chào mừng bạn đến với học viện Rikkei Academy");
+let confirmation = confirm("Bạn đã đủ 18 tuổi chưa!");
+if (confirmation) {
+    console.log("Yes");
+} else {
+    console.log("No");
+}
